04c51a0d3a838f06cb7d59fa2e33300e6eacb212
jorgelda <jorgeluiz112233@gmail.com>
2023-02-10 15:48:24 -0300

    develop - initial css changes

--- src/screens/Informations/index.tsx
+++ src/screens/Informations/index.tsx
@@ -1,8 +1,8 @@
 // STYLES
 import * as Style from './styles';
 
-export const Informations = () => {
-  const teste = 'Informations';
-
-  return <Style.Container>{teste}</Style.Container>;
-};
+export const Informations = () => (
+  <Style.Container>
+    <h2>Informações</h2>
+  </Style.Container>
+);
--- src/screens/MaintenancesPlan/index.tsx
+++ src/screens/MaintenancesPlan/index.tsx
@@ -2,6 +2,8 @@
 import * as Style from './styles';
 
 export const MaintenancesPlan = () => {
+  // redirecionar pro site da easy caso nao tenha token etc... ver com o augusto
+
   const teste = 'MaintenancesPlan';
 
   return <Style.Container>{teste}</Style.Container>;
