53d3586d2de31160978573bc9be88f895b4cf27a
jorgelda <jorgeluiz112233@gmail.com>
2023-02-13 16:45:26 -0300

    SA-525 - area do sindico estatica

--- src/screens/BuildingManagerArea/index.tsx
+++ src/screens/BuildingManagerArea/index.tsx
@@ -1,5 +1,6 @@
 // STYLES
 import { Button } from '../../components/Buttons/Button';
+import { EventTag } from '../../components/EventTag';
 import { Select } from '../../components/Inputs/Select';
 import * as Style from './styles';
 
@@ -12,5 +13,137 @@ export const BuildingManagerArea = () => (
       <Select label="Status" />
       <Button type="button" label="Filtrar" />
     </Style.FilterWrapper>
+    <Style.Kanban>
+      <Style.KanbanCard>
+        <h5>Pendentes</h5>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+      </Style.KanbanCard>
+
+      <Style.KanbanCard>
+        <h5>Vencidas</h5>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+        <Style.MaintenanceInfo>
+          <h6>
+            {' '}
+            Sistemas Hidrossanitários Hidrossanitários Hidrossanitários Hidrossanitários
+            HidrossanitáriosHidrossanitários
+          </h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+      </Style.KanbanCard>
+
+      <Style.KanbanCard>
+        <h5>Concluídas</h5>
+        <Style.MaintenanceInfo>
+          <span>
+            <h6>Sistemas Hidrossanitáriosdsada da</h6>
+            <EventTag status="overdue" />
+          </span>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+        <Style.MaintenanceInfo>
+          <h6>Sistemas Hidrossanitários</h6>
+          <p className="p2">
+            Verificar as tubulações de água potável e servida para detectar obstruções, falhas,
+            entupimentos e problemas de fixação. Reconstruir a sua integridade, se necessário.
+          </p>
+          <p className="p2">tagzinha</p>
+        </Style.MaintenanceInfo>
+      </Style.KanbanCard>
+    </Style.Kanban>
   </Style.Container>
 );
--- src/screens/BuildingManagerArea/styles.tsx
+++ src/screens/BuildingManagerArea/styles.tsx
@@ -5,6 +5,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: ${theme.size.sm};
+  height: 100%;
 `;
 
 export const FilterWrapper = styled.div`
@@ -22,9 +23,66 @@ export const FilterWrapper = styled.div`
     max-width: 100%;
     grid-template-columns: 1fr;
 
+    background-color: ${theme.color.white};
+    padding: ${theme.size.sm};
+    border-radius: ${theme.size.xxsm};
+
     > :last-child {
       margin-left: auto;
       margin-top: ${theme.size.xsm};
+    }
+  }
+`;
+
+export const Kanban = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: ${theme.size.sm};
+  overflow-x: auto;
+  height: 100%;
+
+  scrollbar-width: none;
+  scrollbar-color: transparent;
+
+  ::-webkit-scrollbar {
+    width: 0;
+    background: transparent;
+  }
+`;
+
+export const KanbanCard = styled.div`
+  background-color: ${theme.color.white};
+  padding: ${theme.size.md};
+  border-radius: ${theme.size.xxsm};
+  min-width: 340px;
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.size.xsm};
+
+  @media (max-width: 900px) {
+    padding: ${theme.size.sm};
+  }
+`;
+
+export const MaintenanceInfo = styled.div`
+  padding: ${theme.size.sm};
+  background-color: ${theme.color.gray0};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.04);
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.size.xsm};
+
+  h6 {
+    word-break: break-word;
+  }
+
+  > span {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+
+    > h6 {
+      max-width: calc(100% - 82px);
     }
   }
 `;
