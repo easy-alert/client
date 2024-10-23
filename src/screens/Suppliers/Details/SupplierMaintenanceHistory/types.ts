a2c5868a5001677c9278191d46587b2330cf06ae
Guilherme Roos <guilherme.roosr@gmail.com>
2024-10-23 11:47:25 -0300

    refactor: Remove unused imports and variables in SupplierMaintenanceHistory component

--- src/screens/Suppliers/Details/SupplierMaintenanceHistory/types.ts
+++ src/screens/Suppliers/Details/SupplierMaintenanceHistory/types.ts
@@ -1,31 +1,6 @@
-export interface IMaintenanceReportData {
-  id: string;
-  activity: string;
-  buildingName: string;
-  categoryName: string;
-  cost: number | null;
-  element: string;
-  source: string;
-  maintenanceHistoryId: string;
-  notificationDate: string;
-  resolutionDate: string | null;
-  dueDate: string;
-  responsible: string | null;
-  status: 'completed' | 'expired' | 'pending' | 'overdue';
-  type: 'common' | 'occasional' | null;
-  inProgress: boolean;
-  maintenanceObservation: string | null;
-  reportObservation: string | null;
-  expectedDueDate?: string;
-  expectedNotificationDate?: string;
-  isFuture?: boolean;
+import type { IMaintenanceReportData } from '@customTypes/IMaintenanceReportData';
 
-  images: {
-    url: string;
-  }[];
-
-  annexes: {
-    url: string;
-    name: string;
-  }[];
+export interface ISupplierMaintenanceHistory {
+  maintenancesHistory: IMaintenanceReportData[];
+  getMaintenanceHistory: () => void;
 }
