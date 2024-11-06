611059126cfe4c45f9260bb318c8616c30826212
Guilherme Roos <guilherme.roosr@gmail.com>
2024-11-06 01:44:22 -0300

    feat: refactor getTicketsByBuildingNanoId to use ITicketFilter and update response structure

--- src/services/apis/getTicketsByBuildingNanoId.ts
+++ src/services/apis/getTicketsByBuildingNanoId.ts
@@ -3,13 +3,12 @@ import { Api } from '@services/api';
 import { handleToastify } from '@utils/toastifyResponses';
 
 import type { IResponse } from '@customTypes/IResponse';
-import type { ITicket, ITicketStatus } from '@customTypes/ITicket';
+import type { ITicket } from '@customTypes/ITicket';
+import { ITicketFilter } from '@screens/Tickets';
 
 interface IGetTickets {
   buildingNanoId: string;
-  statusName?: string;
-  initialCreatedAt?: string;
-  finalCreatedAt?: string;
+  filter: ITicketFilter;
   page?: number;
   take?: number;
 }
@@ -17,30 +16,30 @@ interface IGetTickets {
 interface IResponseGetTicketsByBuildingNanoId extends IResponse {
   data: {
     buildingName: string;
-    status: ITicketStatus[];
     tickets: ITicket[];
-    ticketsCount: number;
+    filterOptions: {
+      years: string[];
+      months: string[];
+    };
   };
 }
 
 export const getTicketsByBuildingNanoId = async ({
   buildingNanoId,
-  statusName = '',
-  initialCreatedAt = '',
-  finalCreatedAt = '',
-  page = 0,
-  take = 15,
+  filter,
+  page,
+  take,
 }: IGetTickets) => {
-  const uri = `/tickets/buildings/${buildingNanoId}?statusName=${statusName}&initialCreatedAt=${initialCreatedAt}&finalCreatedAt=${finalCreatedAt}&page=${page}&take=${take}`;
+  const uri = `/tickets/buildings/${buildingNanoId}?year=${filter.year}&month=${filter.month}&status=${filter.status}&placeId=${filter.placeId}&typeId=${filter.serviceTypeId}&page=${page}&take=${take}`;
 
   try {
     const response: IResponseGetTicketsByBuildingNanoId = await Api.get(uri);
 
-    const { buildingName, status, tickets, ticketsCount } = response.data;
+    const { buildingName, tickets, filterOptions } = response.data;
 
-    return { buildingName, status, tickets, ticketsCount };
+    return { buildingName, tickets, filterOptions };
   } catch (error: any) {
     handleToastify(error);
-    return { buildingName: '', status: [], tickets: [], ticketsCount: 0 };
+    return { buildingName: '', tickets: [], filterOptions: { years: [], months: [] } };
   }
 };
