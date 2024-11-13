d732c3d53ec0be51c4e5407819834516accda638
Guilherme Roos <guilherme.roosr@gmail.com>
2024-11-13 14:58:35 -0300

    feat: refactor ModalCreateTicket component to improve modal state management and enhance code organization

--- src/screens/Tickets/ModalCreateTicket/styles.tsx
+++ src/screens/Tickets/ModalCreateTicket/styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 import { theme } from '../../../styles/theme';
 
 export const Container = styled.div`
@@ -11,4 +12,21 @@ export const FrequencyWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: ${theme.size.xsm};
+`;
+
+export const Row = styled.div<{ disabled?: boolean }>`
+  display: flex;
+  flex-direction: column;
+  gap: ${theme.size.xxsm};
+`;
+
+export const FileAndImageRow = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  gap: ${theme.size.xsm};
+
+  > .p2 {
+    opacity: 0.7;
+  }
 `;
