b5d71d4d99080d85beae59123f811c05501164d7
Guilherme Roos <guilherme.roosr@gmail.com>
2025-02-19 06:25:51 -0300

    feat: enhance Modal component with delete functionality and customizable properties

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,31 +1,48 @@
+// GLOBAL ASSETS
+import { icon } from '@assets/icons';
+
+// GLOBAL UTILS
+import { query } from '@utils/functions';
+
 // UI
 import * as Style from './styles';
-// ICONS
-import { icon } from '../../assets/icons';
 
 // TYPES
 import { IModal } from './utils/types';
 import { theme } from '../../styles/theme';
 import { IconButton } from '../Buttons/IconButton';
 
-export const Modal = ({ children, setModal, title, bodyWidth }: IModal) => (
+export const Modal = ({
+  id = 'background',
+  children,
+  title,
+  bodyWidth,
+  closeOutside = true,
+  deleteButton = false,
+  setModal,
+  handleDelete,
+}: IModal) => (
   <Style.Background
-    id="background"
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    id={id}
     onMouseDown={(evt: any) => {
-      if (evt.target.id === 'background') setModal(false);
+      if (evt.target.id === id && !query.get('flow') && closeOutside) setModal(false);
     }}
   >
     <Style.Body bodyWidth={bodyWidth}>
       <Style.Header>
         <h2>{title}</h2>
-        <IconButton
-          icon={icon.x}
-          color={theme.color.primary}
-          onClick={() => {
-            setModal(false);
-          }}
-        />
+
+        <Style.IconsContainer>
+          {deleteButton && (
+            <IconButton
+              icon={icon.grayTrash}
+              color={theme.color.primary}
+              onClick={() => handleDelete && handleDelete(true)}
+            />
+          )}
+
+          <IconButton icon={icon.x} color={theme.color.primary} onClick={() => setModal(false)} />
+        </Style.IconsContainer>
       </Style.Header>
       {children}
     </Style.Body>
