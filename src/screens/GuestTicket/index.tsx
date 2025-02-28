b79ef225f4083d9ed0a15054e9fb005dd0f8aed0
Guilherme Roos <guilherme.roosr@gmail.com>
2025-02-28 15:08:09 -0300

    refactor: update import path for ModalTicketDetails component in GuestTicket

--- src/screens/GuestTicket/index.tsx
+++ src/screens/GuestTicket/index.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 
-import ModalTicketDetails from '@screens/Tickets/ModalTicketDetails';
+import ModalTicketDetails from '@components/ModalTicketDetails';
 
 export const GuestTicket = () => {
   const { ticketId } = useParams() as { ticketId: string };
